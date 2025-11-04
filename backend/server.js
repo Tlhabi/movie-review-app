a5aae859d8a47289e50304b37692dd3a6267ae9b
Tlhabi <sbijotlhabi@gmail.com>
2025-11-04 18:26:51 +0200

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://reviews-app-25.web.app', 'https://review-app-25.firebaseapp.com'],
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
